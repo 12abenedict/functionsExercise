471cbe6d490b667284d5a3d33a4a3e4568c58d8c
Austin Benedict <astone@Austins-MBP.attlocal.net>
2020-11-02 13:32:42 -0600

    completed exercise

--- functionsExercise.js
+++ functionsExercise.js
@@ -25,19 +25,23 @@ console.log(notWolfin);
 
 // 4
 function halloween(){
-    console.log(`October 31st`);
-    return;
+    return `October 31st`;
     
 }
-halloween();
+console.log(halloween());
 
 // 5
 
 function isEnoughCandy(pieces){
     if (pieces >= 31){
         return true;
-    }
+    } else {
     return false;
+    }
+
+    // Or you can use a ternary
+
+    // return (pieces >= 31 ? true : false);
 }
 
 let enough = isEnoughCandy(31);
@@ -47,9 +51,20 @@ console.log(notEnough);
 
 // 6
 function candyCounter(num){
-    console.log(num);
+    let total = 0;
+    for (n of num){total = total + n;
+    }
+
+    // OR you can use a for loop
+    // for(i = 0; i <num.length; i++){
+    //     total = total + num[i];
+    // }
+    return total;
 }
-candyCounter([2 + 3 + 15 + 8]);
+
+candyCounter ([2, 3, 4, 5]);
+candyCounter([2, 3, 4, 5, 6, 6]);
+
 
 // 7
 const hauntedMansion = function (){
@@ -59,32 +74,65 @@ hauntedMansion();
 
 // 8
 ghostbusters = () => `Who You Gonna Call?`;
-const call = ghostbusters();
-console.log(call);
+console.log(ghostbusters());
 
 // 9 NO
-// 10 NO
+// 10 YES
 
 // 11a
 const costumes = [`Ghost`, `Hobo`, `Superman`];
 // 11b
+// "anonymous arrow function"
 const upperCostumes = costumes.map(costumes => costumes.toUpperCase());
 console.log(upperCostumes);
 
 // BONUS
 
 // 12a
-let halloweenSongs1 = [`Moster Mash`, `Thriller`];
+const halloweenSongs1 = [`Moster Mash`, `Thriller`];
 // 12b
-let halloweenSongs2 = [`I Put A Spell On You`, `This Is Halloween`, `Ghostbusters`];
+const halloweenSongs2 = [`I Put A Spell On You`, `This Is Halloween`, `Ghostbusters`];
 // 12c
+// MY ORIGINAL WAY (MESSY AND INCOMPLETE)
+// function halloweenJukebox(songs){
+//     Math.floor(Math.random() * songs.length)
+    
+// }
 
-function halloweenJukebox(halloweenSongs1){
-    return halloweenSongs1[Math.floor(Math.random()*halloweenSongs1.length)];
+// console.log(halloweenJukebox(halloweenSongs1));
+
+// function halloweenJukebox(halloweenSongs2){
+//     return halloweenSongs2[Math.floor(Math.random()*halloweenSongs2.length)];
     
     
+// }
+
+// console.log(halloweenJukebox(halloweenSongs2));
+
+function halloweenJukebox(songs){
+    const randomIndex = Math.floor(Math.random() * songs.length);
+    const randomSong = songs[randomIndex];
+    console.log(randomSong);
+}
+halloweenJukebox(halloweenSongs1)
+halloweenJukebox(halloweenSongs2)
+
+
+// OR
+// function halloweenJukebox(songs){
+//     console.log(songs(Math.floor(Math.random()* songs.length)));
+    
+// }
+
+// 13
+function caps(string){
+    let str = "";
+    for (i = 0; i <= string.length; i++){
+        str += i % 2 == 0 ? string.charAt(i).toUpperCase() :
+        string.charAt(i);
+    
+    }
+    return str;
 }
 
-console.log(halloweenJukebox(halloweenSongs1));
-
-// 12d
+console.log(caps(`abcdef`));
